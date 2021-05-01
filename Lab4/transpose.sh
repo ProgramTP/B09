b120a8ee726a3132b8f598c86c961801d72a0e5f
ProgramTP <58482879+ProgramTP@users.noreply.github.com>
2021-04-30 20:36:27 -0400

    Add files via upload

---
+++ Lab4/transpose.sh
@@ -0,0 +1,31 @@
+tgt="$2"
+src="$1"
+if [ "$#" -ne 2 ]; then
+    echo "Enter two arguments"
+    exit 1
+fi
+if [ -d "$tgt" ]; then
+    echo "target exists"
+    exit 2
+fi
+mkdir "$tgt"
+
+createDir(){
+    for i in "$1"/* ; do
+        for j in "$i"/* ; do
+            x=`basename "$j"`
+            y=`basename "$i"`
+            cd "$tgt"
+	    if [ ! -d "$tgt"/"$x"/* ]; then
+                mkdir "$x"
+	    fi
+            cd "$x"
+            mkdir "$y"
+            cd ..
+            cd ..
+            cp -R "$src"/"$y"/"$x"/* "$tgt"/"$x"/"$y"
+        done
+    done
+}
+
+createDir "$src"
